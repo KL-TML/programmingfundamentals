5b20d61cd6ea7fa0cd651c2f500af3790d758293
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-02 17:12:03 -0400

    exercise4

--- exercise.rb
+++ exercise.rb
@@ -23,12 +23,10 @@ fav_colours = [:blue, :green, :yellow, :red]
 # your favourite movie names and their year of creation
 # three cities of the world and their population
 # the names of your siblings/cousins/friends and their ages
-
 words_def = {:travel => "to go places", :war => "to battle", :paint => "art using a brush"}
 fav_movies = {:superbad => "2005", :avengers => "2010", :x_men => "2001"}
 cities_pop = {:toronto => "3500000", :houston => "4000000", :boston => "10000000"}
 siblings = {:roy => "32", :carter => "15", :anthony => "29"}
-
 
 # Exercise 1
 
@@ -36,7 +34,7 @@ print coin_flip
 print fav_colours[0]
 print siblings.sort
 siblings[:baby] ="0"
-puts fav_movies[:avengers]
+puts fav_movies[:avengmoers]
 
 # Exercise 2
 
@@ -47,3 +45,49 @@ print cities_pop[:toronto]
 artists.each do |artist|
   puts "I think #{artist} is great."
 end
+
+# Exercise 3
+
+  # Question 1
+print artists[0]
+print artists[1]
+
+
+  # Question 2
+  # For each of your favourite movies, print out a sentence about when the movie was released. For example:
+  # Avatar came out in 2009.
+
+  fav_movies.each do |movie, year_of_release|
+    puts "#{movie} came out in #{year_of_release}"
+  end
+
+  # Question 3 Sort and reverse the array of ages of your family
+
+  print siblings.sort_by {|name, age| age}.reverse ##***** found this formula on stackoverflow
+
+
+  # Ex3 Q4
+  fav_movies["Beauty and the Beast"] = [1991, 2017]
+  puts "============"
+  puts fav_movies
+  puts "============"
+
+# Exercise 4
+
+  # Ex4 Q1
+  siblings_under_age_30 = siblings.select do |name, age_of_name|
+    age_of_name.to_i < 30
+  end
+  puts "============"
+  puts siblings_under_age_30
+  puts "============"
+
+
+  # Ex4 Q2
+
+  # siblings.select {|name, age| puts "#{age}" if age == age.max end}
+  puts "============"
+  print siblings.max
+  puts "============"
+
+  # Ex4 Q3
